ca8fc5f81ce86c7a196d890936f7b2f4cb673aa0
unknown <ibrahimsamar858@gmail.com>
2021-01-16 20:22:42 +0200

    ok

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 import socket 
 import threading
 
-HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "good bye"
-thisdict = {
-  "hi": "hi",
-  "hello": "hey samar",
-  "what is your name": "telemedicine",
-  "hey server": "hey client"
+questuions = {
+  "hello": "Hey Samar, I can help you if you're feeling unwell",
+  "i have a headache": "I see. Would you mind answering a few questions about your head?",
+  "sure": "okay let's start",
+  "hi": "Hi again, Samar"
 }
 
 
@@ -23,27 +22,17 @@ def handle_client(conn, addr):
 
     connected = True
     while connected:
-        #msg_length = conn.recv(HEADER).decode(FORMAT)
-        #if msg_length:
-            #msg_length = int(msg_length)
         
         msg = conn.recv(1024).decode(FORMAT)
         if msg == DISCONNECT_MESSAGE:
             conn.send("okay bye bye".encode(FORMAT))
             connected = False
-            #break 
+            
         
         print(f"[{addr}] {msg}")
 
-        #if msg == 'what is your name':
-            #conn.send("telemedicine".encode(FORMAT))
-        #elif msg =='hi' or msg == 'Hi'or  msg == 'HI':
-            #conn.send("hello samar".encode(FORMAT))
-        #else:
-        #if msg == 'Hey server':
-            #conn.send("hey client".encode(FORMAT))
-        if msg in thisdict:
-            conn.send(thisdict.get(msg).encode(FORMAT))
+        if msg in questuions:
+            conn.send(questuions.get(msg).encode(FORMAT))
         else:
             conn.send("not available".encode(FORMAT))
         
